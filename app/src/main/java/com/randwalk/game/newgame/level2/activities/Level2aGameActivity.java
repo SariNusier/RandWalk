fff9a1b6ac704af63f1bb51dd70f753dc3642689
Sari Nusier <sari@sqhive.london>
2015-08-07 20:37:29 +0300

    WOP. Level 2a.

--- app/src/main/java/com/randwalk/game/newgame/level2/activities/Level2aGameActivity.java
+++ app/src/main/java/com/randwalk/game/newgame/level2/activities/Level2aGameActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.graphics.Point;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -13,6 +14,7 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.randwalk.game.R;
+import com.randwalk.game.newgame.level2.views.Level2aPathView;
 import com.randwalk.game.newgame.level2.views.TFView;
 
 import java.util.ArrayList;
@@ -21,6 +23,8 @@ public class Level2aGameActivity extends Activity {
 
     View dnaView;
     View startAreaView;
+    View rightSideView, leftSideView;
+    Level2aPathView pathView;
     RelativeLayout mainLayout;
     Animator.AnimatorListener animatorListener;
     int drawIndex;
@@ -35,6 +39,9 @@ public class Level2aGameActivity extends Activity {
         dnaView = findViewById(R.id.level2a_dna_view);
         startAreaView = findViewById(R.id.level2a_startarea_view);
         mainLayout = (RelativeLayout) findViewById(R.id.level2a_mainlayout);
+        rightSideView = findViewById(R.id.level2a_right_view);
+        leftSideView = findViewById(R.id.level2a_left_view);
+        pathView = (Level2aPathView) findViewById(R.id.level2a_path_view);
         tfViews = new ArrayList<>();
 
         animatorListener = new Animator.AnimatorListener() {
@@ -45,16 +52,29 @@ public class Level2aGameActivity extends Activity {
 
             @Override
             public void onAnimationEnd(Animator animation) {
+                int[] location = new int[2];
+
+                dnaView.getLocationOnScreen(location);
+                float dna_X = location[0];
+                float dna_Y = location[1];
+                float dna_W = tfViews.get(drawIndex).getWidth();
+                float dna_H = tfViews.get(drawIndex).getHeight();
+                float tfView_X = tfViews.get(drawIndex).getX()+tfViews.get(drawIndex).getWidth()/2;
+                float tfView_Y = tfViews.get(drawIndex).getY()+tfViews.get(drawIndex).getHeight()/2;
+
+                if(tfView_Y >= dna_Y){
+                    if(tfView_X >= dna_W/3 && tfView_X <= (2*dna_W)/3)
+                        insideActiveRegion();
+                    else
+                        outsideActiveRegion();
+                    drawIndex++;
+                }
+
                 if(tfViews.get(drawIndex).getCoordinates().y<mainLayout.getHeight()
                         && tfViews.get(drawIndex).getCoordinates().x<mainLayout.getWidth()){
-                    tfViews.get(drawIndex).step();
-                    tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(100).setListener(animatorListener);
-                }
-                else
-                {
-                    drawIndex++;
-                    tfViews.get(drawIndex).step();
-                    tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(100).setListener(animatorListener);
+                    tfViews.get(drawIndex).step(leftSideView,rightSideView);
+                    tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(1).setListener(animatorListener);
+                    pathView.drawLine(tfViews.get(drawIndex).getPrevCoordinates(),tfViews.get(drawIndex).getCoordinates());
                 }
             }
 
@@ -73,7 +93,7 @@ public class Level2aGameActivity extends Activity {
 
     public void placeTF(MotionEvent event){
         if(event.getY() <= startAreaView.getHeight() && tfViews.size()<10 && !walking) {
-            tfViews.add(new TFView(this, new Point(startAreaView.getHeight() / 2, (int) event.getY())));
+            tfViews.add(new TFView(this, new Point((int) event.getX(), startAreaView.getHeight() / 2)));
             RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(50, 50);
             params.leftMargin = Math.round(event.getX());
             params.topMargin = startAreaView.getHeight() / 2;
@@ -86,10 +106,13 @@ public class Level2aGameActivity extends Activity {
             Toast.makeText(this, "WALKING", Toast.LENGTH_LONG).show();
             walking = true;
             drawIndex = 0;
-          //  for (drawIndex = 0; drawIndex < tfViews.size()-1; drawIndex++){
-                tfViews.get(drawIndex).step();
-                tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(100).setListener(animatorListener);
-            //    }
+            tfViews.get(drawIndex).step(leftSideView,rightSideView);
+            tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(1).setListener(animatorListener);
+            pathView.drawLine(tfViews.get(drawIndex).getPrevCoordinates(),tfViews.get(drawIndex).getCoordinates());
+
         }
     }
+
+    public void insideActiveRegion(){}
+    public void outsideActiveRegion(){}
 }
--- app/src/main/java/com/randwalk/game/newgame/level2/views/TFView.java
+++ app/src/main/java/com/randwalk/game/newgame/level2/views/TFView.java
@@ -11,12 +11,14 @@ import com.randwalk.game.R;
 
 import java.util.Random;
 
-/**
- * Created by sari on 17/03/15.
- */
 public class TFView extends View {
     Point coordinates;
     Point prevCoordinates;
+    int sig_Y=15;//this is stdev for normal distribution along Y axes
+    int d=3;//this is step along x axis
+    int random_X,random_Y;
+    boolean bounced = false;
+
     public TFView(Context context, AttributeSet attrs) {
         super(context, attrs);
         Drawable background = this.getResources().getDrawable(R.drawable.cell1);
@@ -42,13 +44,64 @@ public class TFView extends View {
         this.coordinates = coordinates;
     }
 
-    public void step(){
-        Random rand = new Random();
-        prevCoordinates = new Point(coordinates.x,coordinates.y);
-        Log.d("INSIDE TFView","THIS IS THE PREV"+prevCoordinates);
-        coordinates.x = coordinates.x + rand.nextInt(20);
-        coordinates.y = coordinates.y + rand.nextInt(61)-30;
-        Log.d("INSIDE TFView","THIS IS AFTER GENERATION"+coordinates+" "+prevCoordinates);
+    public void step(View leftSide, View rightSide) {
+        if (!bounced){
+            Random rand = new Random();
+            int[] location= new int[2];
+            int bound;
+            prevCoordinates = new Point(coordinates.x, coordinates.y);
+            Log.d("INSIDE TFView", "THIS IS THE PREV" + prevCoordinates);
+            random_Y = d;//(int)Math.abs(Math.floor(rand.nextGaussian()*20)); //generates two random numbers for X and Y
+            random_X = (int) Math.floor(rand.nextGaussian() * sig_Y); //was 30/60 but I think 20/40 looks better
+            rightSide.getLocationOnScreen(location);
+            if(coordinates.x + random_X >= location[0]){
+                //bounce to the left
+                bound = location[0];
+                coordinates.x = bound;
+                coordinates.y = coordinates.y + random_Y/2;
+                bounced = true;
+            }
+            else {
+                leftSide.getLocationOnScreen(location);
+                if(coordinates.x +random_X <= location[0]+leftSide.getWidth()){
+                    bound = location[0]+leftSide.getWidth();
+                    coordinates.x = bound;
+                    coordinates.y = coordinates.y +random_Y/2;
+                    bounced = true;
+                } else {
+                    coordinates.x = coordinates.x + random_X;
+                    coordinates.y = coordinates.y + random_Y;
+                    Log.d("INSIDE TFView", "THIS IS AFTER GENERATION" + coordinates + " " + prevCoordinates);
+                }
+            }
+        } else {
+            coordinates.x = prevCoordinates.x;
+            coordinates.y = coordinates.y + random_Y/2;
+            bounced = false;
+        }
+    }
+
+    public void bounceStep(){
+        if(!bounced)
+        {
+            Random rand = new Random();
+            prevCoordinates = new Point(coordinates.x,coordinates.y);
+            Log.d("INSIDE TFView","THIS IS THE PREV"+prevCoordinates);
+
+            random_Y = d;//(int)Math.abs(Math.floor(rand.nextGaussian()*20)); //generates two random numbers for X and Y
+            random_X = (int)Math.floor(rand.nextGaussian()*sig_Y); //was 30/60 but I think 20/40 looks better
+            coordinates.x = coordinates.x + random_X;
+            coordinates.y = coordinates.y + random_Y/2;
+            Log.d("INSIDE TFView","THIS IS AFTER GENERATION"+coordinates+" "+prevCoordinates);
+            bounced = true;
+        }
+        else
+        {
+            coordinates.x = coordinates.x + random_X;
+            coordinates.y = coordinates.y + random_Y/2;
+            bounced = false;
+        }
+
     }
 
 
