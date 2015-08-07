2c68710b080f36d58af5b3a8272c1ed2880128d6
Sari Nusier <sari@sqhive.london>
2015-08-07 12:17:32 +0300

    WOP. Level 2a.

--- app/src/main/java/com/randwalk/game/newgame/level2/activities/Level2aGameActivity.java
+++ app/src/main/java/com/randwalk/game/newgame/level2/activities/Level2aGameActivity.java
@@ -1,40 +1,95 @@
 package com.randwalk.game.newgame.level2.activities;
 
+import android.animation.Animator;
+import android.app.Activity;
+import android.graphics.Point;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.randwalk.game.R;
+import com.randwalk.game.newgame.level2.views.TFView;
 
-public class Level2aGameActivity extends ActionBarActivity {
+import java.util.ArrayList;
+
+public class Level2aGameActivity extends Activity {
+
+    View dnaView;
+    View startAreaView;
+    RelativeLayout mainLayout;
+    Animator.AnimatorListener animatorListener;
+    int drawIndex;
+    ArrayList<TFView> tfViews;
+
+    boolean walking = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level2a_game);
+        dnaView = findViewById(R.id.level2a_dna_view);
+        startAreaView = findViewById(R.id.level2a_startarea_view);
+        mainLayout = (RelativeLayout) findViewById(R.id.level2a_mainlayout);
+        tfViews = new ArrayList<>();
+
+        animatorListener = new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if(tfViews.get(drawIndex).getCoordinates().y<mainLayout.getHeight()
+                        && tfViews.get(drawIndex).getCoordinates().x<mainLayout.getWidth()){
+                    tfViews.get(drawIndex).step();
+                    tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(100).setListener(animatorListener);
+                }
+                else
+                {
+                    drawIndex++;
+                    tfViews.get(drawIndex).step();
+                    tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(100).setListener(animatorListener);
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        };
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_level2a_game, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void placeTF(MotionEvent event){
+        if(event.getY() <= startAreaView.getHeight() && tfViews.size()<10 && !walking) {
+            tfViews.add(new TFView(this, new Point(startAreaView.getHeight() / 2, (int) event.getY())));
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(50, 50);
+            params.leftMargin = Math.round(event.getX());
+            params.topMargin = startAreaView.getHeight() / 2;
+            mainLayout.addView(tfViews.get(tfViews.size() - 1), params);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void startWalk(){
+        if(tfViews.size()>=5 && !walking) {
+            Toast.makeText(this, "WALKING", Toast.LENGTH_LONG).show();
+            walking = true;
+            drawIndex = 0;
+          //  for (drawIndex = 0; drawIndex < tfViews.size()-1; drawIndex++){
+                tfViews.get(drawIndex).step();
+                tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(100).setListener(animatorListener);
+            //    }
+        }
     }
 }
---
+++ app/src/main/java/com/randwalk/game/newgame/level2/views/Level2bMainLayout.java
@@ -0,0 +1,80 @@
+package com.randwalk.game.newgame.level2.views;
+
+import android.animation.Animator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.v4.view.MotionEventCompat;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.randwalk.game.Activities.Level2BActivity;
+import com.randwalk.game.R;
+import com.randwalk.game.newgame.level2.activities.Level2aGameActivity;
+
+import java.util.Random;
+
+/**
+ * Created by sari on 17/03/15.
+ */
+public class Level2bMainLayout extends RelativeLayout implements View.OnTouchListener{
+
+    GestureDetector gd;
+    Level2aGameActivity parentActivity;
+    Random rand;
+    Animator.AnimatorListener animatorListener;
+
+    public Level2bMainLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        gd = new GestureDetector(new GestureListener());
+        parentActivity = (Level2aGameActivity) context;
+        rand = new Random();
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gd.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+
+
+//    private void startDrawingWalk() {
+    //    parentActivity.startDrawingWalk();
+  //  }
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener{
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            if(MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                //if (event.getY()<=findViewById(R.id.level2b_startarea_view).getHeight()) {
+                    parentActivity.placeTF(event);
+                //}
+
+            }
+            return true;
+        }
+        // event when double tap occurs
+        @Override
+        public boolean onDoubleTap(MotionEvent event) {
+            parentActivity.startWalk();
+            return true;
+        }
+
+    }
+}
+
+
