b1268012eabe78c948d8e5f02484f3fb177ae4d9
Sari Nusier <sari@sqhive.london>
2015-08-08 13:13:10 +0300

    WOP. Level 2a.

--- app/src/main/java/com/randwalk/game/newgame/level2/activities/Level2aGameActivity.java
+++ app/src/main/java/com/randwalk/game/newgame/level2/activities/Level2aGameActivity.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 
 public class Level2aGameActivity extends Activity {
 
-    View dnaView;
+    View dnaLeft,dnaMid,dnaRight;
     View startAreaView;
     View rightSideView, leftSideView;
     Level2aPathView pathView;
@@ -36,7 +36,9 @@ public class Level2aGameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level2a_game);
-        dnaView = findViewById(R.id.level2a_dna_view);
+        dnaLeft = findViewById(R.id.level2a_dna_left_view);
+        dnaMid = findViewById(R.id.level2a_dna_mid_view);
+        dnaRight = findViewById(R.id.level2a_dna_right_view);
         startAreaView = findViewById(R.id.level2a_startarea_view);
         mainLayout = (RelativeLayout) findViewById(R.id.level2a_mainlayout);
         rightSideView = findViewById(R.id.level2a_right_view);
@@ -54,28 +56,35 @@ public class Level2aGameActivity extends Activity {
             public void onAnimationEnd(Animator animation) {
                 int[] location = new int[2];
 
-                dnaView.getLocationOnScreen(location);
+                dnaMid.getLocationOnScreen(location);
                 float dna_X = location[0];
                 float dna_Y = location[1];
-                float dna_W = tfViews.get(drawIndex).getWidth();
-                float dna_H = tfViews.get(drawIndex).getHeight();
+                float dna_W = dnaMid.getWidth();
+                float dna_H = dnaMid.getHeight();
                 float tfView_X = tfViews.get(drawIndex).getX()+tfViews.get(drawIndex).getWidth()/2;
                 float tfView_Y = tfViews.get(drawIndex).getY()+tfViews.get(drawIndex).getHeight()/2;
 
                 if(tfView_Y >= dna_Y){
-                    if(tfView_X >= dna_W/3 && tfView_X <= (2*dna_W)/3)
+                    if(tfView_X >= dna_X && tfView_X <= dna_W+dna_X)
                         insideActiveRegion();
                     else
                         outsideActiveRegion();
-                    drawIndex++;
-                }
+                    if(drawIndex == tfViews.size()-1)
+                        finishWalk();
+                    else{
+                        drawIndex++;
+                        tfViews.get(drawIndex).step(leftSideView,rightSideView);
+                        tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(1).setListener(animatorListener);
+                        pathView.drawLine(tfViews.get(drawIndex).getPrevCoordinates(),tfViews.get(drawIndex).getCoordinates());
+                    }
 
-                if(tfViews.get(drawIndex).getCoordinates().y<mainLayout.getHeight()
-                        && tfViews.get(drawIndex).getCoordinates().x<mainLayout.getWidth()){
+                } else
+                {
                     tfViews.get(drawIndex).step(leftSideView,rightSideView);
                     tfViews.get(drawIndex).animate().x(tfViews.get(drawIndex).getCoordinates().x).y(tfViews.get(drawIndex).getCoordinates().y).setDuration(1).setListener(animatorListener);
                     pathView.drawLine(tfViews.get(drawIndex).getPrevCoordinates(),tfViews.get(drawIndex).getCoordinates());
                 }
+
             }
 
             @Override
@@ -113,6 +122,9 @@ public class Level2aGameActivity extends Activity {
         }
     }
 
-    public void insideActiveRegion(){}
-    public void outsideActiveRegion(){}
+    public void insideActiveRegion(){
+        Toast.makeText(this,"In :)",Toast.LENGTH_SHORT).show();
+    }
+    public void outsideActiveRegion(){Toast.makeText(this,"Out :(",Toast.LENGTH_SHORT).show();}
+    public void finishWalk(){Toast.makeText(this,"Done!",Toast.LENGTH_SHORT).show();}
 }
